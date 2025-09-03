39f1e19b0163b89153b56cc1d815614437b5fe2e
Eoous <38656355+Eoous@users.noreply.github.com>
2025-09-04 01:35:28 +0800

    basic client

--- client.go
+++ client.go
@@ -1,65 +1,76 @@
 package openroutergo
 
 import (
+	"encoding/json"
+	"errors"
+
 	"github.com/go-resty/resty/v2"
 )
 
 const (
 	baseUrl = "https://openrouter.ai/api/v1"
+
+	defaultUserAgent = "openrouter-go (https://github.com/Eoous/openrouter-go)"
+	defaultSiteName  = "openrouter-go)"
+	defaultSiteUrl   = "https://github.com/Eoous/openrouter-go)"
 )
 
 type Client struct {
-	resty *resty.Client
+	Client *resty.Client
 }
 
 func NewClient() *Client {
-	client := resty.New().SetBaseURL(baseUrl)
-	//client.SetHeader("Authorization", "Bearer "+apiKey)
+	client := resty.New().
+		SetBaseURL(baseUrl).
+		SetHeader("User-Agent", defaultUserAgent)
 
-	return &Client{
-		resty: client,
-	}
+	return (&Client{Client: client}).
+		WithSiteName(defaultSiteName).
+		WithSiteUrl(defaultSiteUrl)
 }
 
 func (c *Client) WithAuth(key string) *Client {
-	c.resty.SetHeader("Authorization", "Bearer "+key)
+	c.Client.SetHeader("Authorization", "Bearer "+key)
 
 	return c
 }
 
 // WithSiteUrl sets site url for rankings on openrouter.ai.
 func (c *Client) WithSiteUrl(url string) *Client {
-	c.resty.SetHeader("HTTPReferer", url)
+	c.Client.SetHeader("HTTP-Referer", url)
 
 	return c
 }
 
 // WithSiteName sets site name for rankings on openrouter.ai.
 func (c *Client) WithSiteName(name string) *Client {
-	c.resty.SetHeader("X-Title", name)
+	c.Client.SetHeader("X-Title", name)
 
 	return c
 }
 
-func (c *Client) Completion() (*Client, error) {
-	a :=
-		`{
-			"model": "google/gemini-2.5-flash-image-preview",
-			"messages": [
-				{
-					"role": "user",
-					"content": "Generate a beautiful sunset over mountains"
-				}
-			],
-			"modalities": ["image", "text"]
-	}`
-
-	r, err := c.resty.R().SetBody(a).Post("/chat/completions")
+func (c *Client) Completion(param OpenRouterParams) (*OpenRouterResponse, error) {
+	r, err := c.Client.R().
+		SetBody(param).
+		Post("/chat/completions")
 	if err != nil {
 		return nil, err
 	}
 
-	println(r.String())
+	var resp openRouterResponse
+	err = json.Unmarshal(r.Body(), &resp)
+	if err != nil {
+		return nil, err
+	}
 
-	return c, nil
+	if resp.OpenRouterResponseErr != nil {
+		return nil, errors.New(resp.Error.Message)
+	}
+
+	return resp.OpenRouterResponse, nil
+}
+
+type openRouterResponse struct {
+	*OpenRouterResponse
+	*OpenRouterResponseErr
 }
